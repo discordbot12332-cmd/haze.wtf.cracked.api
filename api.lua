fac8e88d82c542c3dd2629569c855dca3e5208e3
discordbot12332-cmd <discordbot12332@gmail.com>
2025-12-29 19:59:52 +0800

    Create api.lua

---
+++ api.lua
@@ -0,0 +1,23 @@
+-- Haze Loader v2.3
+-- https://haze.wtf
+
+print("���� Haze Loader initialized...")
+
+_G.HAZE_VERSION = "mjoi70sb"
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local function fetchScript()
+    local gameId = tostring(game.GameId)
+    local version = _G.HAZE_VERSION
+    local token = "d3e62dfed4ce4759fadbc2d66975dd97733bf98f7a414844bfca20e0e9d84a05"
+    local url = "https://haze.wtf/api/script/" .. version .. "/" .. gameId .. "?token=" .. token
+
+    print("��摱 Fetching script for game: " .. gameId)
+    local response = game:HttpGet(url)
+    loadstring(response)()
+end
+
+fetchScript()
